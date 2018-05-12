d207f6916d8ca560c3a9b0c922b92dc4137555c5
jul vinc <juliovich@protonmail.ch>
2018-05-12 21:47:18 +0200

    Rails new with frontend and form gems

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,17 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+
+  end
+end
